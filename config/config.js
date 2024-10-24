805cd7f2f667441154490ae8be1536728d5a7a46
Emmanuel Arceo <133131186+TheSCodex@users.noreply.github.com>
2024-10-24 13:43:41 -0500

    setting up migrations

--- config/config.js
+++ config/config.js
@@ -17,10 +17,8 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    use_env_variable: 'DB_STRING',
+    dialect: 'postgres',
   },
+  
 };
