86f8a72a96e0ed72496d9734d82dda9f704d5b1a
Emmanuel Arceo <133131186+TheSCodex@users.noreply.github.com>
2024-10-24 14:56:23 -0500

    db syncing fix

--- seeders/runSeeder.js
+++ seeders/runSeeder.js
@@ -1,17 +1,20 @@
-const Sequelize = require('sequelize');
-const connection = require('../src/db.js');
-const roleSeeder = require('./20241019214127-seed-roles.js');   
+const Sequelize = require("sequelize");
+const connection = require("../src/db.js");
+const roleSeeder = require("./20241019214127-seed-roles.js");
 
 const runSeeder = async () => {
   try {
     await connection.authenticate();
-    console.log('Database connected successfully.');
+    console.log("Database connected successfully.");
 
-    // Ensure to pass the Sequelize reference to the up method
-    await roleSeeder.up({ queryInterface: connection.getQueryInterface(), Sequelize });
-    console.log('Seeding completed successfully!');
+    // Get the queryInterface from the connection
+    const queryInterface = connection.getQueryInterface();
+
+    // Pass the queryInterface and Sequelize reference to the up method
+    await roleSeeder.up(queryInterface, Sequelize);
+    console.log("Seeding completed successfully!");
   } catch (error) {
-    console.error('Error seeding data:', error);
+    console.error("Error seeding data:", error);
   } finally {
     await connection.close();
   }
