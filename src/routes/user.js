1f451e21caba762aff7abdacbb4b535068627c77
Emmanuel Arceo <133131186+TheSCodex@users.noreply.github.com>
2024-10-27 00:06:46 -0500

    user updating

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/user.js');
 const userRoutes = express.Router();
 
 userRoutes.get('/list-user', controller.getAllUsers);
+userRoutes.post('/get-user', controller.getUserById);
 userRoutes.post('/create-user', controller.createUser);
 userRoutes.post('/login-user', controller.loginUser);
 userRoutes.post('/block-user', controller.blockUser);
